0a227b34216ba8f1ddb88899bc72d32059b6151b
angeldavid218 <angeldavid218@gmail.com>
2018-07-06 14:34:43 -0500

    migracion approval

---
+++ database/migrations/2018_07_03_163707_add_approval_field_to_template_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddApprovalFieldToTemplateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('template', function (Blueprint $table) {
+            $table->tinyInteger('approval')->after('url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
