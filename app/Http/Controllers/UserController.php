5d66fa7b3194db78864a87a8acea682070bcfe56
angeldavid218 <angeldavid218@gmail.com>
2018-07-26 01:24:32 -0500

    user validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,13 +17,16 @@ class UserController extends Controller
      */
     public function index()
     {
-        $items = User::latest('updated_at')->get();
+        $users = User::latest('updated_at')->get();
         $id = Auth::id();
-        $creditos=DB::table("user_credit")->where('user_id','=',$id)->sum('credits');
-        $discounts=DB::table("discounts")->where('user_id','=',$id)->sum('credits');
-        $total = $creditos-$discounts;
-
-        return view('admin.users.index', compact('items','total'));
+        foreach($users as $user) {
+            $creditosUser = DB::table("user_credit")->where('user_id' ,'=', $user->id)->sum('credits');
+            $discountsUser = DB::table("discounts")->where('user_id' ,'=', $user->id)->sum('credits');
+            $totalCreditsUser = $creditosUser - $discountsUser;
+            $user->totalCredits = $totalCreditsUser;
+        }
+       
+        return view('admin.users.index', compact('users','total'));
     }
 
     /**
