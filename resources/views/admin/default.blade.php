51149b4601b1af46eac397ba0ee71c11696a7d25
Isaac Sánchez <isanchez@ia-society.com>
2018-07-26 12:19:12 -0500

    Styles changes on web panel were added

--- resources/views/admin/default.blade.php
+++ resources/views/admin/default.blade.php
@@ -15,6 +15,8 @@
   <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.2/css/responsive.dataTables.min.css">
+  <!-- font-awesome icon -->
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
 </head>
 <body class="app">
