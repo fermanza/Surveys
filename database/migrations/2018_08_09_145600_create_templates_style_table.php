3dfb473ae4cf85f3f0aa53cff180e09338316dbb
Ad Gtz <fernando.manzano@vanillasys.com>
2018-08-09 19:03:12 -0500

    Templates

--- database/migrations/2018_08_09_145600_create_templates_style_table.php
+++ database/migrations/2018_08_09_145600_create_templates_style_table.php
@@ -19,41 +19,46 @@ class CreateTemplatesStyleTable extends Migration
             $table->string('background')->nullable();
             $table->string('font_type')->nullable();
             $table->string('font_color')->unique();
+            $table->string('bar_color')->unique();
             $table->timestamps();
         });
 
         \DB::table('templates_style')->insert(
             array(
                 'name' => 'Default',
-                'background' => 'none',
-                'small_image' => '/public/images/backgrounds/abstract_small.jpg',
-                'font_type' => 'none',
-                'font_color' => 'none',
+                'background' => '',
+                'small_image' => '/public/images/templates/default_small.png',
+                'font_type' => '',
+                'font_color' => '',
+                'bar_color' => '',
             ),
             array(
                 'name' => 'Abstract',
-                'background' => '/public/images/backgrounds/abstract.jpg',
-                'small_image' => '/public/images/backgrounds/abstract_small.jpg',
-                'font_type' => 'none',
-                'font_color' => 'green',
+                'background' => '/public/images/templates/abstract_background.png',
+                'small_image' => '/public/images/templates/abstract_small.jpg',
+                'font_type' => '',
+                'font_color' => '#3d7641',
+                'bar_color' => '#5c9f30',
             ),
             array(
                 'name' => 'Blue',
-                'background' => '/public/images/backgrounds/blue.jpg',
-                'small_image' => '/public/images/backgrounds/blue_small.jpg',
-                'font_type' => 'none',
-                'font_color' => 'blue',
+                'background' => '/public/images/templates/blue_background.jpg',
+                'small_image' => '/public/images/templates/blue_small.jpg',
+                'font_type' => 'Theserif',
+                'font_color' => '#0d2a3d',
+                'bar_color' => '#66a7de',
             ),
             array(
                 'name' => 'New',
-                'background' => '/public/images/backgrounds/new.jpg',
-                'small_image' => '/public/images/backgrounds/new_small.jpg',
+                'background' => '/public/images/templates/new.jpg',
+                'small_image' => '/public/images/templates/new_small.jpg',
                 'font_type' => 'none',
                 'font_color' => 'new',
+                'bar_color' => 'new',
             )
         );
     }
-
+    
     /**
      * Reverse the migrations.
      *
@@ -61,6 +66,6 @@ class CreateTemplatesStyleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('templates_style');
     }
 }
